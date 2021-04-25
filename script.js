5020cfd4b4aaa474cf73cb6e638656cdbd6ec7ce
sally <sallyquosto@icloud.com>
2021-04-25 19:36:11 -0400

    completed lab one

---
+++ script.js
@@ -0,0 +1,29 @@
+const name = 'Mitch Cuckovich';
+const age = 25;
+const birthday = 'January 24';
+const detroitGC = true;
+const lifeEvents = ['I was born in Troy, Michigan.', 'I went to Hope College', 'I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.', 'Im a graduate of the front-end bootcamp']
+
+
+    if (detroitGC === true) {
+        console.log(`My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am curently ${age} years old and my birthday is on ${birthday}.`)
+    } else {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am curently ${age} years old and my birthday is on ${birthday}.`)
+    }
+
+    for (let i = 0; i < lifeEvents.length; i++) {
+        console.log(lifeEvents[i])
+    }
+
+    let counter = 0;
+    while(true) {
+        let randomNumber = Math.floor(Math.random() * 10) + 1;
+        if (randomNumber !== 5) {
+            counter++;
+            console.log(`${randomNumber} !== 5`)
+        } else {
+            counter++;
+            console.log(`5 === 5 it took ${counter} to randomly generate the number 5`)
+            break;
+        }
+    }
